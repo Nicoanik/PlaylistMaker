db243c684d7d125f9dc713d650387740374dcd71
Sergei <Nicoanik@hotmail.com>
2025-06-26 10:13:34 +0300

    Sprint 10

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -1,20 +1,27 @@
 package com.example.playlistmaker
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
     private val trackNameView: TextView = itemView.findViewById(R.id.track_name)
     private val artistNameView: TextView = itemView.findViewById(R.id.artist_name)
     private val trackTimeView: TextView = itemView.findViewById(R.id.track_time)
-    private val artworkUrl10View: TextView = itemView.findViewById(R.id.album_cover)
+    private val artworkUrl100View: ImageView = itemView.findViewById(R.id.album_cover)
 
     fun bind(model: Track) {
         trackNameView.text = model.trackName
         artistNameView.text = model.artistName
         trackTimeView.text = model.trackTime
-        artworkUrl10View.text = model.artworkUrl100
+        Glide.with(itemView)
+            .load(model.artworkUrl100)
+            .centerCrop()
+            .transform(RoundedCorners(10))
+            .into(artworkUrl100View)
     }
 }
