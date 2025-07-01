60181403259132536345b6961c9b7201cfce035c
Sergei <Nicoanik@hotmail.com>
2025-07-01 15:05:53 +0300

    Sprint 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,5 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation("com.github.bumptech.glide:glide:4.14.2")
     annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
+    implementation("com.google.code.gson:gson:2.10")
 }
