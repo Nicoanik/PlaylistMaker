54f6c05962be0eb0726d5a5302ea59de9cf893a5
Sergei <Nicoanik@hotmail.com>
2025-07-09 20:24:19 +0300

    Sprint 11 09.07.2025 20:23

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -61,41 +61,41 @@ class SearchActivity : AppCompatActivity() {
         tracksList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         tracksList.adapter = adapter
 
-//        queryInput.setOnEditorActionListener { _, actionId, _ ->
-//            if (actionId == EditorInfo.IME_ACTION_DONE) {
-//                // ВЫПОЛНЯЙТЕ ПОИСКОВЫЙ ЗАПРОС ЗДЕСЬ
-//                if (queryInput.text.isNotEmpty()) {
-//                    itunesService.search(queryInput.text.toString()).enqueue(object :
-//                        Callback<TracksResponse> {
-//                        override fun onResponse(call: Call<TracksResponse>,
-//                                                response: Response<TracksResponse>
-//                        ) {
-//                            if (response.code() == 200) {
-//                                tracks.clear()
-//                                if (response.body()?.results?.isNotEmpty() == true) {
-//                                    tracks.addAll(response.body()?.results!!)
-//                                    adapter.notifyDataSetChanged()
-//                                }
-//                                if (tracks.isEmpty()) {
-//                                    showMessage(getString(R.string.nothing_found), "")
-//                                } else {
-//                                    showMessage("", "")
-//                                }
-//                            } else {
-//                                showMessage(getString(R.string.something_went_wrong), response.code().toString())
-//                            }
-//                        }
-//
-//                        override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
-//                            showMessage(getString(R.string.something_went_wrong), t.message.toString())
-//                        }
-//
-//                    })
-//                }
-//                true
-//            }
-//            false
-//        }
+        queryInput.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // ВЫПОЛНЯЙТЕ ПОИСКОВЫЙ ЗАПРОС ЗДЕСЬ
+                if (queryInput.text.isNotEmpty()) {
+                    itunesService.search(queryInput.text.toString()).enqueue(object :
+                        Callback<TracksResponse> {
+                        override fun onResponse(call: Call<TracksResponse>,
+                                                response: Response<TracksResponse>
+                        ) {
+                            if (response.code() == 200) {
+                                tracks.clear()
+                                if (response.body()?.results?.isNotEmpty() == true) {
+                                    tracks.addAll(response.body()?.results!!)
+                                    adapter.notifyDataSetChanged()
+                                }
+                                if (tracks.isEmpty()) {
+                                    showMessage(getString(R.string.nothing_found), "")
+                                } else {
+                                    showMessage("", "")
+                                }
+                            } else {
+                                showMessage(getString(R.string.something_went_wrong), response.code().toString())
+                            }
+                        }
+
+                        override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
+                            showMessage(getString(R.string.something_went_wrong), t.message.toString())
+                        }
+
+                    })
+                }
+                true
+            }
+            false
+        }
 
 //        val inputEditText = findViewById<EditText>(R.id.inputEditText)
 //        inputEditText.setText(editText)
@@ -126,48 +126,48 @@ class SearchActivity : AppCompatActivity() {
         queryInput.addTextChangedListener(textWatcher)
 
 //        val recycler = findViewById<RecyclerView>(R.id.track_list)
-
-        val tracks = listOf(
-            Track("Smells Like Teen Spirit",
-                "Nirvana",
-                "5:01",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-            Track("Billie Jean",
-                "Michael Jackson",
-                "4:35",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-            Track("Stayin' Alive",
-                "Bee Gees",
-                "4:10",
-                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-            Track("Whole Lotta Love",
-                "Led Zeppelin",
-                "5:33",
-                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-            Track("Sweet Child O'Mine",
-                "Guns N' Roses",
-                "5:03",
-                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-        )
+//
+//        val tracks = listOf(
+//            Track("Smells Like Teen Spirit",
+//                "Nirvana",
+//                "5:01",
+//                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
+//            Track("Billie Jean",
+//                "Michael Jackson",
+//                "4:35",
+//                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
+//            Track("Stayin' Alive",
+//                "Bee Gees",
+//                "4:10",
+//                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
+//            Track("Whole Lotta Love",
+//                "Led Zeppelin",
+//                "5:33",
+//                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
+//            Track("Sweet Child O'Mine",
+//                "Guns N' Roses",
+//                "5:03",
+//                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
+//        )
 
 //        recycler.layoutManager = LinearLayoutManager(this)
 //        recycler.adapter = TrackAdapter(tracks)
     }
 
-//    private fun showMessage(text: String, additionalMessage: String) {
-//        if (text.isNotEmpty()) {
-//            placeholderMessage.visibility = View.VISIBLE
-//            tracks.clear()
-//            adapter.notifyDataSetChanged()
-//            placeholderMessage.text = text
-//            if (additionalMessage.isNotEmpty()) {
-//                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
-//                    .show()
-//            }
-//        } else {
-//            placeholderMessage.visibility = View.GONE
-//        }
-//    }
+    private fun showMessage(text: String, additionalMessage: String) {
+        if (text.isNotEmpty()) {
+            placeholderMessage.visibility = View.VISIBLE
+            tracks.clear()
+            adapter.notifyDataSetChanged()
+            placeholderMessage.text = text
+            if (additionalMessage.isNotEmpty()) {
+                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+                    .show()
+            }
+        } else {
+            placeholderMessage.visibility = View.GONE
+        }
+    }
 
     private var editText: String = TEXT_DEF
 
