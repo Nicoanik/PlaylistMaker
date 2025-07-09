bd9c8297f0834652257b461c8dc8b3c830a71473
Sergei <Nicoanik@hotmail.com>
2025-07-09 19:47:26 +0300

    Sprint 11 09.07.2025 19:46

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -8,6 +8,7 @@ import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -31,8 +32,9 @@ class SearchActivity : AppCompatActivity() {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    private val itunesService =retrofit.create(ItunesApi::class.java)
+    private val itunesService = retrofit.create(ItunesApi::class.java)
 
+    private lateinit var placeholderMessage: TextView
     private lateinit var queryInput: EditText
     private lateinit var tracksList: RecyclerView
 
@@ -50,7 +52,8 @@ class SearchActivity : AppCompatActivity() {
             insets
         }
 
-        queryInput = findViewById(R.id.inputEditText)
+        placeholderMessage = findViewById(R.id.placeholderMessage)
+        queryInput = findViewById(R.id.queryInput)
         tracksList = findViewById(R.id.tracks_list)
 
         adapter.tracks = tracks
@@ -58,44 +61,45 @@ class SearchActivity : AppCompatActivity() {
         tracksList.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         tracksList.adapter = adapter
 
-        queryInput.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_DONE) {
-                // ВЫПОЛНЯЙТЕ ПОИСКОВЫЙ ЗАПРОС ЗДЕСЬ
-                if (queryInput.text.isNotEmpty()) {
-                    itunesService.search(queryInput.text.toString()).enqueue(object :
-                        Callback<TracksResponse> {
-                        override fun onResponse(call: Call<TracksResponse>,
-                                                response: Response<TracksResponse>
-                        ) {
-                            if (response.code() == 200) {
-                                tracks.clear()
-                                if (response.body()?.results?.isNotEmpty() == true) {
-                                    tracks.addAll(response.body()?.results!!)
-                                    adapter.notifyDataSetChanged()
-                                }
-                                if (tracks.isEmpty()) {
-                                    showMessage(getString(R.string.nothing_found), "")
-                                } else {
-                                    showMessage("", "")
-                                }
-                            } else {
-                                showMessage(getString(R.string.something_went_wrong), response.code().toString())
-                            }
-                        }
-
-                        override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
-                            showMessage(getString(R.string.something_went_wrong), t.message.toString())
-                        }
-
-                    })
-                }
-                true
-            }
-            false
-        }
+//        queryInput.setOnEditorActionListener { _, actionId, _ ->
+//            if (actionId == EditorInfo.IME_ACTION_DONE) {
+//                // ВЫПОЛНЯЙТЕ ПОИСКОВЫЙ ЗАПРОС ЗДЕСЬ
+//                if (queryInput.text.isNotEmpty()) {
+//                    itunesService.search(queryInput.text.toString()).enqueue(object :
+//                        Callback<TracksResponse> {
+//                        override fun onResponse(call: Call<TracksResponse>,
+//                                                response: Response<TracksResponse>
+//                        ) {
+//                            if (response.code() == 200) {
+//                                tracks.clear()
+//                                if (response.body()?.results?.isNotEmpty() == true) {
+//                                    tracks.addAll(response.body()?.results!!)
+//                                    adapter.notifyDataSetChanged()
+//                                }
+//                                if (tracks.isEmpty()) {
+//                                    showMessage(getString(R.string.nothing_found), "")
+//                                } else {
+//                                    showMessage("", "")
+//                                }
+//                            } else {
+//                                showMessage(getString(R.string.something_went_wrong), response.code().toString())
+//                            }
+//                        }
+//
+//                        override fun onFailure(call: Call<TracksResponse>, t: Throwable) {
+//                            showMessage(getString(R.string.something_went_wrong), t.message.toString())
+//                        }
+//
+//                    })
+//                }
+//                true
+//            }
+//            false
+//        }
 
 //        val inputEditText = findViewById<EditText>(R.id.inputEditText)
 //        inputEditText.setText(editText)
+        queryInput.setText(editText)
 
         val backButton = findViewById<ImageView>(R.id.back_button_search)
         backButton.setOnClickListener {
@@ -123,47 +127,47 @@ class SearchActivity : AppCompatActivity() {
 
 //        val recycler = findViewById<RecyclerView>(R.id.track_list)
 
-//        val trackList = listOf(
-//            Track("Smells Like Teen Spirit",
-//                "Nirvana",
-//                "5:01",
-//                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-//            Track("Billie Jean",
-//                "Michael Jackson",
-//                "4:35",
-//                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-//            Track("Stayin' Alive",
-//                "Bee Gees",
-//                "4:10",
-//                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-//            Track("Whole Lotta Love",
-//                "Led Zeppelin",
-//                "5:33",
-//                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-//            Track("Sweet Child O'Mine",
-//                "Guns N' Roses",
-//                "5:03",
-//                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
-//        )
+        val tracks = listOf(
+            Track("Smells Like Teen Spirit",
+                "Nirvana",
+                "5:01",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
+            Track("Billie Jean",
+                "Michael Jackson",
+                "4:35",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
+            Track("Stayin' Alive",
+                "Bee Gees",
+                "4:10",
+                "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
+            Track("Whole Lotta Love",
+                "Led Zeppelin",
+                "5:33",
+                "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
+            Track("Sweet Child O'Mine",
+                "Guns N' Roses",
+                "5:03",
+                "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
+        )
 
 //        recycler.layoutManager = LinearLayoutManager(this)
 //        recycler.adapter = TrackAdapter(tracks)
-
-        private fun showMessage(text: String, additionalMessage: String) {
-            if (text.isNotEmpty()) {
-                placeholderMessage.visibility = View.VISIBLE
-                tracks.clear()
-                adapter.notifyDataSetChanged()
-                placeholderMessage.text = text
-                if (additionalMessage.isNotEmpty()) {
-                    Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
-                        .show()
-                }
-            } else {
-                placeholderMessage.visibility = View.GONE
-            }
-        }
     }
+
+//    private fun showMessage(text: String, additionalMessage: String) {
+//        if (text.isNotEmpty()) {
+//            placeholderMessage.visibility = View.VISIBLE
+//            tracks.clear()
+//            adapter.notifyDataSetChanged()
+//            placeholderMessage.text = text
+//            if (additionalMessage.isNotEmpty()) {
+//                Toast.makeText(applicationContext, additionalMessage, Toast.LENGTH_LONG)
+//                    .show()
+//            }
+//        } else {
+//            placeholderMessage.visibility = View.GONE
+//        }
+//    }
 
     private var editText: String = TEXT_DEF
 
--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -2,14 +2,18 @@ package com.example.playlistmaker
 
 import android.content.Context
 import android.util.TypedValue
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
-class TrackViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+class TrackViewHolder(parent: ViewGroup): RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context)
+        .inflate(R.layout.track_view, parent, false)) {
 
     private val trackNameView: TextView = itemView.findViewById(R.id.track_name)
     private val artistNameView: TextView = itemView.findViewById(R.id.artist_name)
