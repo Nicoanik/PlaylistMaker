cd2bc4d02c83a754598b1d2b4140a3dca7cdc41c
Sergei <Nicoanik@hotmail.com>
2025-09-29 07:50:49 +0300

    Sprint 16 add binding PlayerActivity

--- app/src/main/java/com/example/playlistmaker/player/ui/activity/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/activity/PlayerActivity.kt
@@ -14,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
+import com.example.playlistmaker.databinding.ActivityPlayerBinding
 import com.example.playlistmaker.search.domain.models.Track
 import com.example.playlistmaker.search.domain.models.dpToPx
 import com.example.playlistmaker.search.domain.models.timeConversion
@@ -23,6 +24,8 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 class PlayerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPlayerBinding
 
     enum class PlayerState(val state: Int) {
         DEFAULT(0),
@@ -34,41 +37,18 @@ class PlayerActivity : AppCompatActivity() {
     private var mediaPlayer = MediaPlayer()
     private var playerState = PlayerState.DEFAULT
 
-    private lateinit var backButton: ImageView
-    private lateinit var playButton: ImageButton
-    private lateinit var playbackProgress: TextView
-    private lateinit var ivAlbumCover: ImageView
-    private lateinit var tvTrackName: TextView
-    private lateinit var tvArtistName: TextView
-    private lateinit var tvTrackTime: TextView
-    private lateinit var tvCollectionName: TextView
-    private lateinit var tvReleaseDate: TextView
-    private lateinit var tvPrimaryGenreName: TextView
-    private lateinit var tvCountry: TextView
-
     private val mainHandler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_player)
+        binding = ActivityPlayerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_player)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        backButton = findViewById(R.id.back_button_audio_player)
-        playButton = findViewById(R.id.play_button)
-        playbackProgress = findViewById(R.id.playback_progress)
-        ivAlbumCover = findViewById(R.id.iv_album_cover)
-        tvTrackName = findViewById(R.id.tv_track_name)
-        tvArtistName = findViewById(R.id.tv_artist_name)
-        tvTrackTime = findViewById(R.id.tv_track_time)
-        tvCollectionName = findViewById(R.id.tv_collection_name)
-        tvReleaseDate = findViewById(R.id.tv_release_date)
-        tvPrimaryGenreName = findViewById(R.id.tv_primary_genre_name)
-        tvCountry = findViewById(R.id.tv_country)
 
         val mediaTrack = Gson().fromJson(intent.getStringExtra(SearchActivity.Companion.TRACK_INTENT), Track::class.java)
 
@@ -77,23 +57,25 @@ class PlayerActivity : AppCompatActivity() {
             .placeholder(R.drawable.album_cover_placeholder)
             .centerCrop()
             .transform(RoundedCorners(dpToPx(8, this)))
-            .into(ivAlbumCover)
-        tvTrackName.text = mediaTrack.trackName
-        tvArtistName.text = mediaTrack.artistName
-        tvTrackTime.text = timeConversion(mediaTrack.trackTime)
-        tvCollectionName.text = mediaTrack.collectionName
-        tvReleaseDate.text = mediaTrack.releaseDate.substring(0, 4)
-        tvPrimaryGenreName.text = mediaTrack.primaryGenreName
-        tvCountry.text = mediaTrack.country
-        playbackProgress.text = PLAYBACK_DEF
+            .into(binding.ivAlbumCover)
+        binding.apply {
+            tvTrackName.text = mediaTrack.trackName
+            tvArtistName.text = mediaTrack.artistName
+            tvTrackTime.text = timeConversion(mediaTrack.trackTime)
+            tvCollectionName.text = mediaTrack.collectionName
+            tvReleaseDate.text = mediaTrack.releaseDate.substring(0, 4)
+            tvPrimaryGenreName.text = mediaTrack.primaryGenreName
+            tvCountry.text = mediaTrack.country
+            playbackProgress.text = PLAYBACK_DEF
+        }
 
-        backButton.setOnClickListener {
+        binding.backButtonAudioPlayer.setOnClickListener {
             finish()
         }
 
         preparePlayer(mediaTrack)
 
-        playButton.setOnClickListener {
+        binding.playButton.setOnClickListener {
             playbackControl()
         }
     }
@@ -105,21 +87,21 @@ class PlayerActivity : AppCompatActivity() {
             playerState = PlayerState.PREPARED
         }
         mediaPlayer.setOnCompletionListener {
-            playButton.setImageResource(R.drawable.play_button_100)
+            binding.playButton.setImageResource(R.drawable.play_button_100)
             playerState = PlayerState.PREPARED
             mainHandler.removeCallbacksAndMessages(null)
-            playbackProgress.text = PLAYBACK_DEF
+            binding.playbackProgress.text = PLAYBACK_DEF
         }
     }
 
     private fun startPlayer() {
         mediaPlayer.start()
-        playButton.setImageResource(R.drawable.pause_button_100)
+        binding.playButton.setImageResource(R.drawable.pause_button_100)
         playerState = PlayerState.PLAYING
         mainHandler.post(
             object : Runnable {
                 override fun run() {
-                    playbackProgress.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(mediaPlayer.currentPosition)
+                    binding.playbackProgress.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(mediaPlayer.currentPosition)
                     mainHandler.postDelayed(this, 500)
                 }
             }
@@ -128,7 +110,7 @@ class PlayerActivity : AppCompatActivity() {
 
     private fun pausePlayer() {
         mediaPlayer.pause()
-        playButton.setImageResource(R.drawable.play_button_100)
+        binding.playButton.setImageResource(R.drawable.play_button_100)
         playerState = PlayerState.PAUSED
         mainHandler.removeCallbacksAndMessages(null)
     }
