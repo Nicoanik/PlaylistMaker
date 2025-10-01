f3efa9ad43802781cadeb157f85b605dd5c9fb49
Sergei <Nicoanik@hotmail.com>
2025-10-01 03:49:44 +0300

    Sprint 16 add SearchViewModel (finish)

---
+++ app/src/main/java/com/example/playlistmaker/search/ui/view_model/HistoryState.kt
@@ -0,0 +1,10 @@
+package com.example.playlistmaker.search.ui.view_model
+
+import com.example.playlistmaker.search.domain.models.Track
+
+sealed interface HistoryState {
+
+    data class GetHistory(val trackHistory: List<Track>) : HistoryState
+
+    data class AddHistory(val tracksHistory: List<Track>) : HistoryState
+}
