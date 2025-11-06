b64520627d0f9707d4075a8beaf191468ba549db
Sergei <Nicoanik@hotmail.com>
2025-11-06 12:42:01 +0300

    Sprint 19 add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,5 +61,7 @@ dependencies {
 
     implementation(libs.koin.android)
 
-    implementation(libs.material.v180)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.fragment.ktx)
 }
