a64ab9a7ed3a9450248ae6c02c378101ab695d3b
Sergei <Nicoanik@hotmail.com>
2025-11-07 20:39:40 +0300

    Sprint 19 change colors BottomNavigationView

--- app/src/main/java/com/example/playlistmaker/root/ui/activity/RootActivity.kt
+++ app/src/main/java/com/example/playlistmaker/root/ui/activity/RootActivity.kt
@@ -3,8 +3,6 @@ package com.example.playlistmaker.root.ui.activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.playlistmaker.R
@@ -19,16 +17,12 @@ class RootActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityRootBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
 
         binding.bottomNavigationView.setupWithNavController(navController)
+
 
     }
 }
