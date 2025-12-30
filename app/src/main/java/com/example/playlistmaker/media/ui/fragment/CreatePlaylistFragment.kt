3370c803328590691809f30b39388ab8db719cac
Sergei <Nicoanik@hotmail.com>
2025-12-30 18:23:33 +0300

    ---

--- app/src/main/java/com/example/playlistmaker/media/ui/fragment/CreatePlaylistFragment.kt
+++ app/src/main/java/com/example/playlistmaker/media/ui/fragment/CreatePlaylistFragment.kt
@@ -1,15 +1,27 @@
 package com.example.playlistmaker.media.ui.fragment
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.FragmentCreatePlaylistBinding
 import com.example.playlistmaker.media.ui.view_model.CreatePlaylistViewModel
+import com.example.playlistmaker.search.domain.models.dpToPx
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.io.File
+import java.io.FileOutputStream
 
 class CreatePlaylistFragment : Fragment() {
 
@@ -21,7 +33,7 @@ class CreatePlaylistFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentCreatePlaylistBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -29,8 +41,47 @@ class CreatePlaylistFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val pickMedia =
+            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+                if (uri != null) {
+                    Glide.with(requireContext())
+                        .load(uri)
+                        .centerCrop()
+                        .transform(RoundedCorners(dpToPx(8, requireContext())))
+                        .into(binding.ivPlaylistCover)
+                }
+            }
+
         binding.backButton.setOnClickListener {
             findNavController().navigateUp()
         }
+
+        binding.ivPlaylistCover.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+//    private fun saveImageToPrivateStorage(uri: Uri) {
+//        //создаём экземпляр класса File, который указывает на нужный каталог
+//        val filePath = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "myplaylists")
+//        //создаем каталог, если он не создан
+//        if (!filePath.exists()){
+//            filePath.mkdirs()
+//        }
+//        //создаём экземпляр класса File, который указывает на файл внутри каталога
+//        val file = File(filePath, "first_cover.jpg")
+//        // создаём входящий поток байтов из выбранной картинки
+//        val inputStream = contentResolver.openInputStream(uri)
+//        // создаём исходящий поток байтов в созданный выше файл
+//        val outputStream = FileOutputStream(file)
+//        // записываем картинку с помощью BitmapFactory
+//        BitmapFactory
+//            .decodeStream(inputStream)
+//            .compress(Bitmap.CompressFormat.JPEG, 30, outputStream)
+//    }
 }
