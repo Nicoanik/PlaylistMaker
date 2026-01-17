29447564ea2d3cc7fb0c20caf8a915d85b0dcdcb
Sergei <Nicoanik@hotmail.com>
2026-01-17 15:01:35 +0300

    ---

---
+++ app/src/main/java/com/example/playlistmaker/PlaylistDetailsFragment.kt
@@ -0,0 +1,30 @@
+package com.example.playlistmaker
+
+import androidx.fragment.app.viewModels
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PlaylistDetailsFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = PlaylistDetailsFragment()
+    }
+
+    private val viewModel: PlaylistDetailsViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // TODO: Use the ViewModel
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_playlist_details, container, false)
+    }
+}
---
+++ app/src/main/java/com/example/playlistmaker/PlaylistDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.playlistmaker
+
+import androidx.lifecycle.ViewModel
+
+class PlaylistDetailsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
