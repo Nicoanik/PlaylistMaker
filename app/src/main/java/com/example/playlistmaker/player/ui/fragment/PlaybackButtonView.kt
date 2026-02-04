360b4c44edf830a486dc80d7f92d46d2582bde34
Sergei <Nicoanik@hotmail.com>
2026-02-04 23:31:47 +0300

    Sprint 24

--- app/src/main/java/com/example/playlistmaker/player/ui/fragment/PlaybackButtonView.kt
+++ app/src/main/java/com/example/playlistmaker/player/ui/fragment/PlaybackButtonView.kt
@@ -1,15 +1,14 @@
 package com.example.playlistmaker.player.ui.fragment
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.graphics.Canvas
 import android.graphics.RectF
 import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 import androidx.annotation.AttrRes
 import androidx.annotation.StyleRes
-import androidx.core.graphics.drawable.toBitmap
 import com.example.playlistmaker.R
 
 class PlaybackButtonView @JvmOverloads constructor(
@@ -22,6 +21,8 @@ class PlaybackButtonView @JvmOverloads constructor(
     private val playButton: Drawable?
     private val pauseButton: Drawable?
     private var imageRect = RectF(0f, 0f, 0f, 0f)
+
+    private var isPlaying = false
 
     init {
         context.theme.obtainStyledAttributes(
@@ -45,10 +46,36 @@ class PlaybackButtonView @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas) {
-        pauseButton?.let {
-            it.setBounds(0,0,width,height)
-            it.draw(canvas)
+        super.onDraw(canvas)
+        val image = if (isPlaying) pauseButton else playButton
+        image?.apply {
+            setBounds(
+                imageRect.left.toInt(),
+                imageRect.top.toInt(),
+                imageRect.right.toInt(),
+                imageRect.bottom.toInt()
+            )
+            draw(canvas)
         }
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                return true
+            }
+
+            MotionEvent.ACTION_UP -> {
+                performClick()
+                return true
+            }
+        }
+        return super.onTouchEvent(event)
+    }
+
+    fun setImage(isPlaying: Boolean) {
+        this.isPlaying = isPlaying
+        invalidate()
     }
 
 }
